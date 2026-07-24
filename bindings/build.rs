b120363806ce67eefceaa6ce18744834d3bc1c62
Silent Spud <1478389+SilentSpud@users.noreply.github.com>
2026-07-24 03:32:48 -0600

    update dependencies and use windows crate directly instead of making bindings

--- bindings/build.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    windows::build! {
-        Windows::Win32::UI::Shell::*,
-        Windows::Win32::UI::WindowsAndMessaging::*,
-        Windows::Win32::System::Registry::*,
-        Windows::Win32::System::Threading::*,
-    };
-}
--- src/win32.rs
+++ src/win32.rs
@@ -1,6 +1,20 @@
-use std::{ffi::c_void, io::{Error, ErrorKind}, mem, path::Path, ptr::null_mut};
+use std::{ffi::c_void, io::{Error, ErrorKind}, mem, path::Path};
 
-use bindings::Windows::Win32::{Foundation::{HANDLE, HINSTANCE, HWND, PWSTR}, System::Registry::{HKEY, HKEY_CLASSES_ROOT, KEY_ALL_ACCESS, KEY_QUERY_VALUE, REG_CREATE_KEY_DISPOSITION, REG_OPTION_NON_VOLATILE, REG_SZ, RRF_RT_REG_SZ, RegCloseKey, RegCreateKeyExW, RegGetValueW, RegOpenKeyExW, RegSetValueExW}, UI::Shell::{CommandLineToArgvW, SHELLEXECUTEINFOW, SHELLEXECUTEINFOW_0, ShellExecuteExW}};
+use windows::{
+    core::{PCWSTR, PWSTR},
+    Win32::{
+        Foundation::ERROR_MORE_DATA,
+        System::Registry::{
+            HKEY, HKEY_CLASSES_ROOT, KEY_ALL_ACCESS, KEY_QUERY_VALUE, REG_CREATE_KEY_DISPOSITION,
+            REG_OPTION_NON_VOLATILE, REG_SZ, RRF_RT_REG_SZ, RegCloseKey, RegCreateKeyExW,
+            RegGetValueW, RegOpenKeyExW, RegSetValueExW,
+        },
+        UI::{
+            Shell::{CommandLineToArgvW, SHELLEXECUTEINFOW, ShellExecuteExW},
+            WindowsAndMessaging::SW_SHOW,
+        },
+    },
+};
 
 // copied from
 // https://github.com/microsoft/windows-samples-rs/blob/master/webview2_win32/src/pwstr.rs
@@ -22,13 +36,23 @@ fn string_from_pwstr(source: PWSTR) -> String {
     }
 }
 
+fn to_wide(input: &str) -> Vec<u16> {
+    let mut res: Vec<u16> = input.encode_utf16().collect();
+    res.push(0);
+    res
+}
+
 pub fn parse_commandline(command_line: &str) -> Result<(String, Vec<String>), Error> {
     let exe: String;
     let mut args: Vec<String> = vec![];
 
     unsafe {
         let mut num_args: i32 = 0;
-        let parsed = CommandLineToArgvW(command_line, &mut num_args);
+        let command_line_w = to_wide(command_line);
+        let parsed = CommandLineToArgvW(PCWSTR(command_line_w.as_ptr()), &mut num_args);
+        if parsed.is_null() {
+            return Err(Error::last_os_error());
+        }
 
         exe = string_from_pwstr(*parsed);
 
@@ -43,32 +67,51 @@ pub fn parse_commandline(command_line: &str) -> Result<(String, Vec<String>), Er
 pub fn get_protocol_handler(protocol: &str) -> Result<String, Error> {
     let result: String;
     unsafe {
-        let mut hkey: HKEY = HKEY::NULL;
-        let sub_key = format!(r"{0}\shell\open\command", protocol);
-        let mut res = RegOpenKeyExW(HKEY_CLASSES_ROOT, sub_key,
-             0, KEY_QUERY_VALUE, & mut hkey);
+        let mut hkey: HKEY = HKEY::default();
+        let sub_key = to_wide(format!(r"{0}\shell\open\command", protocol).as_str());
+        let mut res = RegOpenKeyExW(
+            HKEY_CLASSES_ROOT,
+            PCWSTR(sub_key.as_ptr()),
+            Some(0),
+            KEY_QUERY_VALUE,
+            &mut hkey,
+        );
         if res.0 != 0 {
-            return Err(Error::from_raw_os_error(res.0));
+            return Err(Error::from_raw_os_error(res.0 as i32));
         }
 
         let mut buffer: Vec<u16> = vec![0; 256];
-        let mut data_size: u32 = buffer.capacity() as u32;
+        let mut data_size: u32 = (buffer.capacity() * 2) as u32;
         // preflight to find out the required buffer size
-        res = RegGetValueW(hkey, None, None, RRF_RT_REG_SZ,
-              null_mut(), buffer.as_mut_ptr() as * mut c_void, & mut data_size);
+        res = RegGetValueW(
+            hkey,
+            PCWSTR::null(),
+            PCWSTR::null(),
+            RRF_RT_REG_SZ,
+            None,
+            Some(buffer.as_mut_ptr() as *mut c_void),
+            Some(&mut data_size),
+        );
 
         buffer.resize((data_size / 2 - 1) as usize, 0);
 
         // more data, buffer was too small
-        if res.0 == 234 {
-            res = RegGetValueW(hkey, None, None, RRF_RT_REG_SZ,
-                null_mut(), buffer.as_mut_ptr() as * mut c_void, & mut data_size);
+        if res == ERROR_MORE_DATA {
+            res = RegGetValueW(
+                hkey,
+                PCWSTR::null(),
+                PCWSTR::null(),
+                RRF_RT_REG_SZ,
+                None,
+                Some(buffer.as_mut_ptr() as *mut c_void),
+                Some(&mut data_size),
+            );
             // cut off 0 terminator
             buffer.resize((data_size / 2 - 1) as usize, 0);
         }
 
         if res.0 != 0 {
-            return Err(Error::from_raw_os_error(res.0));
+            return Err(Error::from_raw_os_error(res.0 as i32));
         }
 
         result = String::from_utf16(&buffer).expect("failed to convert registry value");
@@ -76,7 +119,7 @@ pub fn get_protocol_handler(protocol: &str) -> Result<String, Error> {
         res = RegCloseKey(hkey);
 
         if res.0 != 0 {
-            return Err(Error::from_raw_os_error(res.0));
+            return Err(Error::from_raw_os_error(res.0 as i32));
         }
     }
 
@@ -85,80 +128,64 @@ pub fn get_protocol_handler(protocol: &str) -> Result<String, Error> {
 
 pub fn set_protocol_handler(protocol: &str, command: &str) -> Result<(), Error> {
     unsafe {
-        let mut hkey: HKEY = HKEY::NULL;
-        let sub_key = format!(r"{0}\shell\open\command", protocol);
+        let mut hkey: HKEY = HKEY::default();
+        let sub_key = to_wide(format!(r"{0}\shell\open\command", protocol).as_str());
         let mut dispo: REG_CREATE_KEY_DISPOSITION = REG_CREATE_KEY_DISPOSITION(0);
-        let mut res = RegCreateKeyExW(HKEY_CLASSES_ROOT, sub_key,
-            0, None, REG_OPTION_NON_VOLATILE,
-            KEY_ALL_ACCESS, null_mut(), & mut hkey,
-            & mut dispo);
+        let mut res = RegCreateKeyExW(
+            HKEY_CLASSES_ROOT,
+            PCWSTR(sub_key.as_ptr()),
+            Some(0),
+            PWSTR::null(),
+            REG_OPTION_NON_VOLATILE,
+            KEY_ALL_ACCESS,
+            None,
+            &mut hkey,
+            Some(&mut dispo),
+        );
 
         if res.0 != 0 {
-            return Err(Error::from_raw_os_error(res.0));
+            return Err(Error::from_raw_os_error(res.0 as i32));
         }
 
-        let mut command_u16 = vec![];
-        for ch in command.encode_utf16() {
-            // little endian
-            command_u16.push(ch as u8);
-            command_u16.push((ch >> 8) as u8);
-        }
+        let command_u16 = to_wide(command);
+        let command_u8 = std::slice::from_raw_parts(
+            command_u16.as_ptr() as *const u8,
+            command_u16.len() * 2,
+        );
 
-        res = RegSetValueExW(hkey, None, 0, REG_SZ,
-            command_u16.as_ptr(), command_u16.len() as u32);
+        res = RegSetValueExW(hkey, PCWSTR::null(), Some(0), REG_SZ, Some(command_u8));
 
         if res.0 != 0 {
-            return Err(Error::from_raw_os_error(res.0));
+            return Err(Error::from_raw_os_error(res.0 as i32));
         }
 
         res = RegCloseKey(hkey);
 
         if res.0 != 0 {
-            return Err(Error::from_raw_os_error(res.0));
+            return Err(Error::from_raw_os_error(res.0 as i32));
         }
     }
 
     Ok(())
 }
 
-fn to_utf16(input: &str) -> Vec<u16> {
-    let mut res: Vec<u16> = input.encode_utf16().collect();
-    res.push(0);
-    return res;
-}
-
 pub fn spawn_elevated(exe: &str, args: Vec<&str>) -> Result<(), Error> {
     let cwd = Path::new(&exe).parent().unwrap().to_str().unwrap();
 
-    let mut verb: Vec<u16> = to_utf16("runas");
-    let mut file: Vec<u16> = to_utf16(exe);
-    let mut directory: Vec<u16> = to_utf16(cwd);
-    let mut parameters: Vec<u16> = to_utf16(args.join(" ").as_str());
-    let mut class: Vec<u16> = vec![];
+    let verb = to_wide("runas");
+    let file = to_wide(exe);
+    let directory = to_wide(cwd);
+    let parameters = to_wide(args.join(" ").as_str());
 
-    let mut exec_info: SHELLEXECUTEINFOW = SHELLEXECUTEINFOW {
-        cbSize: mem::size_of::<SHELLEXECUTEINFOW>() as u32,
-        lpVerb: PWSTR(verb.as_mut_ptr()),
-        lpFile: PWSTR(file.as_mut_ptr()),
-        lpDirectory: PWSTR(directory.as_mut_ptr()),
-        lpParameters: PWSTR(parameters.as_mut_ptr()),
-        nShow: 1,
-        fMask: 0,
-        hwnd: HWND(0),
-        hInstApp: HINSTANCE(0),
-        lpIDList: 0 as * mut c_void,
-        hkeyClass: HKEY(0),
-        lpClass: PWSTR(class.as_mut_ptr()),
-        Anonymous: SHELLEXECUTEINFOW_0 { hIcon: HANDLE(0) },
-        hProcess: HANDLE(0),
-        dwHotKey: 0,
-    };
+    let mut exec_info = SHELLEXECUTEINFOW::default();
+    exec_info.cbSize = mem::size_of::<SHELLEXECUTEINFOW>() as u32;
+    exec_info.lpVerb = PCWSTR(verb.as_ptr());
+    exec_info.lpFile = PCWSTR(file.as_ptr());
+    exec_info.lpDirectory = PCWSTR(directory.as_ptr());
+    exec_info.lpParameters = PCWSTR(parameters.as_ptr());
+    exec_info.nShow = SW_SHOW.0;
 
     unsafe {
-        if ShellExecuteExW(& mut exec_info).as_bool() {
-            Ok(())
-        } else {
-            Err(Error::new(ErrorKind::Other, "failed to spawn"))
-        }
+        ShellExecuteExW(&mut exec_info).map_err(|e| Error::new(ErrorKind::Other, e.to_string()))
     }
 }
